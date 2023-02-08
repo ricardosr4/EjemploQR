0df85a300500d9948128f079ef25c89bfb518dc0
Ricardo <ricardosr4@gmail.com>
2023-02-08 10:27:22 -0300

    ejemplo de QR

---
+++ app/src/main/java/com/example/ejemploqr/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.ejemploqr
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.ejemploqr.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnScanner.setOnClickListener { initScanner() }
+    }
+
+    private fun initScanner() {
+        IntentIntegrator(this).initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        val result = IntentIntegrator.parseActivityResult(resultCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "cancelado", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "el valor escaneado es ${result.contents}", Toast.LENGTH_SHORT).show()
+
+            }
+        } else {
+
+
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
